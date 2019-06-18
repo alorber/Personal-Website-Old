98dc8247d81a5ca66e6e52b59bc7c1167281c703
Andrew Lorber <andrewlorber@aol.com>
2019-06-17 20:48:40 -0400

    Edits intro text

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
 	const word_loop = new TypeIt("#Intro_Words", {loop: true, autoStart: true, cursor: false})
-						.type("I am a computer engineer at The Cooper Union.")
+						.type("I am a computer engineering major at The Cooper Union.")
 						.pause(2000)
 						.delete()
-						.type("I am interested in becoming a software engineer.")
-						.pause(3000)
+						.type("I hope to become a software engineer, but I am also interested in web development.")
+						.pause(2000)
 						.delete()
-						.type("I am interested in web development.")
-						.pause(3000)
+						.type("Welcome to my site!")
+						.pause(2000)
 
 	new TypeIt("#Intro_Text", {
 		cursor: false,
